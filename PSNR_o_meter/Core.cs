f83e3976362663cd1c92b4a0ac54b285889343de
devpro97 <devpro97@outlook.com>
2019-07-02 15:04:04 +0300

    Created core math and some utils

---
+++ PSNR_o_meter/Core.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSNR_o_meter
+{
+    class Core
+    {
+        public class PSNRmeter
+        {
+            PictureMetrics meter;
+            byte[,] mapLeft, mapRight;
+
+            public PSNRmeter(Bitmap left, Bitmap right)
+            {
+                mapLeft = Utils.RGBtoGrayscale(left);
+                mapRight = Utils.RGBtoGrayscale(right);
+            }
+
+            /// <summary>
+            /// Changes one of pictures in class instance. Use this method to save your time 
+            /// not grayscaling second image one more time.
+            /// </summary>
+            /// <param name="bitmap">New image to be measured</param>
+            /// <param name="isLeft">True if it is left image, false otherwise</param>
+            public void ChangePicture(Bitmap bitmap, bool isLeft)
+            {
+                var map = Utils.RGBtoGrayscale(bitmap);
+                if (isLeft)
+                {
+                    mapLeft = map;
+                }
+                else
+                {
+                    mapRight = map;
+                }
+            }
+
+            /// <summary>
+            /// Measures pictures PSNR
+            /// </summary>
+            /// <returns>Dictionary, which contains all obtained meashures in name-value relations</returns>
+            public Dictionary<string, float> CalculatePSRN()
+            {
+                meter.MeterPictures(mapLeft, mapRight);
+                return meter.metrics;
+            }
+        }
+               
+        public abstract class PictureMetrics
+        {
+            public Dictionary<string, float> metrics = new Dictionary<string, float>(2); //because it'll often have only two meashures
+
+            public abstract void MeterPictures(byte[,] left, byte[,] right);
+        }
+        public class PictureMetricsFull : PictureMetrics
+        {
+            public override void MeterPictures(byte[,] left, byte[,] right)
+            {
+                if (left.GetLength(0) != right.GetLength(0) || left.GetLength(1) != right.GetLength(1))
+                {
+                    throw new Exception($"Wrong pictures size! {left.GetLength(0)}х{left.GetLength(0)} vs {right.GetLength(0)}х{right.GetLength(1)} px");
+                }
+
+                var summsquare = 0f;
+                var max = 0f;
+                for (int i = 0; i < left.GetLength(0); i++)
+                {
+                    for (int j = 0; j < left.GetLength(1); j++)
+                    {
+                        var x = left[i, j];
+                        var y = right[i, j];
+                        summsquare += (float)Math.Pow((x - y), 2);
+                        if (x > max)
+                            max = x;
+                    }
+                }
+                var msq = summsquare / left.Length;
+                var psnr = 10 * (float)Math.Log10(max * max / msq);
+                this.metrics.Add("PSNR", psnr);
+                this.metrics.Add("Standard deviation", msq);
+            }
+        }
+        //    public class PictureMetricsBitwise : PictureMetrics
+    }
+}
---
+++ PSNR_o_meter/Utils.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PSNR_o_meter
+{
+    class Utils
+    {
+        public static unsafe Bitmap RGBtoGrayscaleBitmap(Bitmap bitmap)
+        {
+            var format = bitmap.PixelFormat;
+            if (format != PixelFormat.Format32bppArgb)
+            {
+                throw new BadImageFormatException($"{format} is not supported by this prog!");
+            }
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, format);
+            byte* picStart = (byte*)data.Scan0.ToPointer();
+            byte bytesPerPix = (byte)(data.Stride / data.Width);
+
+            for (short x = 0; x < data.Width; ++x)
+                for (short y = 0; y < data.Height; ++y)
+                {
+                    byte* pix = picStart + x * bytesPerPix + y * data.Stride;
+                    byte gray = RGBToGrayscalePixel(pix);
+                    pix[2] = gray;
+                    pix[1] = gray;
+                    pix[0] = gray;
+                }
+            bitmap.UnlockBits(data);
+
+            return bitmap;
+        }
+        public static unsafe byte[,] RGBtoGrayscale(Bitmap bitmap)
+        {
+            byte[,] mapOfValues = new byte[bitmap.Width, bitmap.Height];
+
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, bitmap.PixelFormat);
+            byte* picStart = (byte*)data.Scan0.ToPointer();
+            byte bytesPerPix = (byte)(data.Stride / data.Width);
+
+            for (short x = 0; x < data.Width; ++x)
+            {
+                for (short y = 0; y < data.Height; ++y)
+                {
+                    byte* pix = picStart + x * bytesPerPix + y * data.Stride;
+                    mapOfValues[x, y] = RGBToGrayscalePixel(pix);
+                }
+            }
+            bitmap.UnlockBits(data);
+
+            return mapOfValues;
+        }
+
+        static Color RGBtoGrayscalePixel(Color col)
+        {
+            byte b = RGBToGrayscalePixel(col);
+            return Color.FromArgb(col.A, b, b, b);
+        }
+        static byte RGBToGrayscalePixel(Color col)
+        {
+            return (byte)(0.3 * col.R + 0.59 * col.G + 0.11 * col.B);
+        }
+        static unsafe byte RGBToGrayscalePixel(byte* pix)
+        {
+            return (byte)(0.3 * pix[1] + 0.59 * pix[2] + 0.11 * pix[3]);
+        }
+        static unsafe byte RGBToGrayscalePixel(byte* pix, byte throwedBits)
+        {
+            byte r = (byte)(pix[1] >> throwedBits);
+            byte g = (byte)(pix[2] >> throwedBits);
+            byte b = (byte)(pix[3] >> throwedBits);
+            return (byte)(0.3 * r + 0.59 * g + 0.11 * b);
+        }
+    }
+}
